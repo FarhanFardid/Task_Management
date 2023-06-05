3eba24285f438cdbc3c20b576a54997b40503e04
FarhanFardid <farhanfardid009@gmail.com>
2023-06-05 21:51:34 +0600

    update

--- src/Pages/Home/Featured.jsx
+++ src/Pages/Home/Featured.jsx
@@ -6,10 +6,10 @@ const Featured = () => {
         <div className="p-5">
             <h2 className="text-4xl font-bold text-black py-5 text-center">Features </h2>
             <div className="grid grid-cols-4 gap-2">
-            <FeatureCard title="Task Creation" content="Ability to create tasks easily and quickly.Task categorization and tagging for better organization and filtering" btnTag="Creation"></FeatureCard>
-            <FeatureCard title="Task Prioritization " content="Tools for setting task priorities and assigning importance levels.Task scheduling and deadline management" btnTag="Prioritize"></FeatureCard>
-            <FeatureCard title="Progress Tracking" content="Visual progress indicators or status updates for tasks and projects.Tracking of completed tasks, pending tasks, and overall project progress" btnTag="Progress"></FeatureCard>
-            <FeatureCard title="Reminders and Notifications" content="Automated reminders for upcoming deadlines or overdue tasks.Notifications for task assignments." btnTag="Reminder"></FeatureCard>
+            <FeatureCard title="Task Create" content="Users can create tasks and added it to the database easily and quickly by completing a form. The added task will automatically appear on the view task table list." btnTag="Creating Task"></FeatureCard>
+            <FeatureCard title="Task View " content="User can view all the tasks they added in the view task page with all the available details." btnTag="Viewing Task"></FeatureCard>
+            <FeatureCard title="Task Update" content="User can update the task status from pending,working,in progress to completed. After clicking on 'mark as completed' button, the status will automatically updated to completed.  " btnTag="Updating Task"></FeatureCard>
+            <FeatureCard title="Task Delete" content="Users can delete any completed or any task in other status.After the deletion the task will be removed from the task table list." btnTag="Deleting Task"></FeatureCard>
             </div>
          
         </div>
